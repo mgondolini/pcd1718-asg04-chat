e62bf612b1e920eb6917387b99a4a39395ecd151
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-03-08 11:31:20 +0100

    added chatroomcontroller.

---
+++ src/main/java/client/ChatRoomController.java
@@ -0,0 +1,13 @@
+package client;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatRoomController implements Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
